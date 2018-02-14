c1dc28d8b3eb36463204bd6de8100d3e31ad2157
Dewantara tirta <dewantara.tirta@gmail.com>
2018-02-15 01:13:52 +0700

    test debug headers

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<h1>Hello world</h1>
 <?php 
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -8,6 +9,7 @@ if( $method == 'POST' ){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
+	$json->debug = getallheaders();
 	$text = $json->result->parameters->text;
 
 	switch($text) {
