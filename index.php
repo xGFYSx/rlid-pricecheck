dbf1320a7910ccb8267b8092c84b66e6f1277e3e
FRDS <agung.frds7@gmail.com>
2018-04-26 20:16:34 +0700

    Fix bug platform #3

--- index.php
+++ index.php
@@ -42,7 +42,6 @@ if( $method )
 					case 'checkprice':
 						$item = new Item;
 						$result = $item->setQuery($json->result->resolvedQuery)
-														->setPlatform($json->result->parameters->platform)
 														->getPrice();
 
 						// echo $response->setText($item->speech,$result)->result();
