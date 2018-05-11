dbb5f97cd400cf50712db62254cd14621a45b2c1
FRDS <agung.frds7@gmail.com>
2018-05-11 23:12:25 +0700

    Test fix

--- rank.php
+++ rank.php
@@ -57,7 +57,7 @@ class Rank
                 //remove platform from string
             foreach ($this->default_platform as $var) {
                 foreach ($var as $varr) {
-                    if( preg_match("/\b".$varr."\b/", $query) ){
+                    if( preg_match("/(\b".$varr."\b,|,\b".$varr."$)/", $query)) {
                         $query = str_replace($varr, '', $query);
                         $this->platform = key($var);
                     }   
