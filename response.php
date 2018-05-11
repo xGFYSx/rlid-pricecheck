c87d31a63cae224d322f25122dbc4a8a1dcbc562
FRDS <agung.frds7@gmail.com>
2018-05-11 23:50:37 +0700

    Maintenance notice

--- response.php
+++ response.php
@@ -12,9 +12,10 @@ class ResponseMsg {
 
   public function setText($speech,$text)
   {
+
     // $this->__set( 'speech', trim(preg_replace('/\s+/', ' ', $speech)) );
-    $this->__set( "\xE2\x9A\xA0 Bot sedang dalam maintenance. Mohon ditunggu ya... \xF4\x80\x82\x92", $speech );
-    $this->__set( "\xE2\x9A\xA0 Bot sedang dalam maintenance. Mohon ditunggu ya... \xF4\x80\x82\x92", $text );
+    $this->__set( 'speech', "\xE2\x9A\xA0 Bot sedang dalam maintenance. Mohon ditunggu ya... \xF4\x80\x82\x92" );
+    $this->__set( 'displayText', "\xE2\x9A\xA0 Bot sedang dalam maintenance. Mohon ditunggu ya... \xF4\x80\x82\x92" );
     return $this;
   }
 
