63a36611b3b98b7b1050fb01f7440ac39977fdff
FRDS <agung.frds7@gmail.com>
2018-05-14 16:46:38 +0700

    error cURL #6

--- rank.php
+++ rank.php
@@ -197,7 +197,7 @@ class Rank
               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
               CURLOPT_CUSTOMREQUEST => 'GET',
               CURLOPT_HTTPHEADER => array(
-                'authorization: $user',
+                'authorization: $key',
               ),
             ));
 
